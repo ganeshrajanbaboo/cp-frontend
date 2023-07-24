c2bdd532b295f10e6e164bd9669385ac1c158d96
toncinta <toncinta@gmail.com>
2023-07-24 11:31:54 +0530

    first commit

---
+++ src/components/TransactionItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Tr, Td } from "@chakra-ui/react";
+
+export default function TransactionItem({ transaction }) {
+  return (
+    <Tr>
+      <Td>{transaction["name"]}</Td>
+      <Td>{transaction["symbol"]}</Td>
+      <Td isNumeric>$ {(transaction["amount"]/100).toLocaleString()}</Td>
+      <Td isNumeric>{transaction["no_of_coins"]}</Td>
+      <Td isNumeric>$ {transaction["price_purchased_at"].toLocaleString()}</Td>
+      <Td isNumeric>{transaction["time_transacted"]}</Td>
+    </Tr>
+  );
+}
